68b193c9dd823b8a35cfd3884f4db854cc3deff5
blesscat <blesscat@gmail.com>
2019-03-06 13:36:59 +0800

    trello report change

--- app/modules/trello_modules.py
+++ app/modules/trello_modules.py
@@ -1,18 +1,39 @@
 from trello import TrelloClient
 from openpyxl import load_workbook
 from copy import copy
-import ipdb
+
+import datetime
 
 blessId = '5bff57cb3b4c0b21262eabc6'
 owenId = '5bff594d317049461251056e'
+claraId = '54b3636bb2b9ce3343113869'
+sunnyId = '5adc58e6bed38687f94a7fb5'
+howardId = '5c6ce68c093e2182061dceb3'
 
-members = [blessId, owenId]
+members = [blessId, owenId, claraId, sunnyId, howardId]
 
 key = 'd19356dd8fdeb53226fc5e3fd4f3ab1b'
 secret = 'b8f25dfe09f13a2ab3318ed14692a1d8f8aad9a1db4c5183021ae9aa5b77fa23'
 
 
-def findInAllBoards(client, nameList, id):
+def calPercentage(card):
+    if not card.checklists:
+        return '0%'
+
+    checklist = card.checklists[0].items    
+    checked = 0
+    for item in checklist:
+        if item['checked']:
+            checked = checked + 1
+
+    if checked:
+        return '{}%'.format((checked / len(checklist)) * 100)
+    else:
+        return '0%'
+
+
+def findInAllBoards(client, nameList, id, percentage):
+    print(id, nameList)
     li = []
     boards = client.list_boards()
     for board in boards:
@@ -20,29 +41,47 @@ def findInAllBoards(client, nameList, id):
             if lis.name in nameList:
                 for card in lis.list_cards():
                     if id in card.member_id:
+
+                        if percentage:
+                            percent = calPercentage(card)
+                        else:
+                            percent = ''
+
                         item = [
                             board.name,
                             '#{}'.format(card.short_id),
-                            card.name
+                            card.name,
+                            percent
                         ]
+                        print(item)
                         li.append(item)
     return li
 
 
-def getAllItems(lists):
+def getAllItems(lists, percentage):
     client = TrelloClient(api_key=key, api_secret=secret)
     dones = []
     for memberId in members:
-        done = findInAllBoards(client, lists, memberId)
+        done = findInAllBoards(client, lists, memberId, percentage)
         dones.append(done)
     return dones
 
 
 def putInXlsx(wb, items, row):
+    today = datetime.date.today()
+    rangeDate = datetime.timedelta(days=6)
+    startDate = today - rangeDate
+    formattedDate = '日 期: {} ~ {}'.format(
+        startDate.strftime('%Y/%m/%d'),
+        today.strftime('%m/%d'),
+    )
     for i in range(len(members)):
         ws = wb.worksheets[i]
         done = items[i]
         cell = copy(ws.cell(row=row, column=1))
+
+        ws.cell(row=2, column=5).value = formattedDate
+
         for item in done:
             print(item)
             ws.insert_rows(row)
@@ -53,22 +92,27 @@ def putInXlsx(wb, items, row):
                 ws.cell(row=row, column=i).number_format = copy(cell.number_format)
                 ws.cell(row=row, column=i).protection = copy(cell.protection)
                 # ws.cell(row=10, column=i).alignment = copy(cell.alignment)
-            for i in range(1, 4):
+
+            for i in range(1, 5):
                 ws.cell(row=row, column=i).value = item[i - 1]
     return wb
 
 
 def run():
-    dones = getAllItems(['已上傳待複驗', '複驗完成'])
-    inProgress = getAllItems(['進行中'])
+    dones = getAllItems(['進行中', '已修改', '已上傳待複驗', '複驗完成'], True)
+    inProgress = getAllItems(['進行中'], False)
 
     filepath = "./report.xlsx"
     wb = load_workbook(filepath)
 
-    wb = putInXlsx(wb, dones, 10)
-    wb = putInXlsx(wb, inProgress, 5)
+    wb = putInXlsx(wb, inProgress, 10)
+    wb = putInXlsx(wb, dones, 5)
 
-    wb.save(filename="./工作週報_Frontend_2019.xlsx")
+    today = datetime.date.today()
+    filename = "./工作週報_Frontend_{}.xlsx".format(
+        today.strftime('%Y%m%d')
+    )
+    wb.save(filename=filename)
 
     print('done')
 
