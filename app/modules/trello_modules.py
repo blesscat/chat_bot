cc5e151ed0c5f4d0bf0021010a9ad8a7dd01566e
blesscat <blesscat@gmail.com>
2019-03-22 15:48:00 +0800

    add ocr

--- app/modules/trello_modules.py
+++ app/modules/trello_modules.py
@@ -3,17 +3,21 @@ from openpyxl import load_workbook
 from copy import copy
 
 import datetime
+import re
 
 blessId = '5bff57cb3b4c0b21262eabc6'
 owenId = '5bff594d317049461251056e'
 claraId = '54b3636bb2b9ce3343113869'
+wilsonId = '5c2c5edca9970548e6e9b78e'
 sunnyId = '5adc58e6bed38687f94a7fb5'
 howardId = '5c6ce68c093e2182061dceb3'
 
-members = [blessId, owenId, claraId, sunnyId, howardId]
+members = [blessId, owenId, claraId, wilsonId, sunnyId, howardId]
 
 key = 'd19356dd8fdeb53226fc5e3fd4f3ab1b'
 secret = 'b8f25dfe09f13a2ab3318ed14692a1d8f8aad9a1db4c5183021ae9aa5b77fa23'
+
+ILLEGAL_CHARACTERS_RE = re.compile(r'[\000-\010]|[\013-\014]|[\016-\037]')
 
 
 def calPercentage(card):
@@ -40,6 +44,7 @@ def findInAllBoards(client, nameList, id, percentage):
         for lis in board.list_lists():
             if lis.name in nameList:
                 for card in lis.list_cards():
+                    print(card, card.member_id)
                     if id in card.member_id:
 
                         if percentage:
@@ -94,7 +99,8 @@ def putInXlsx(wb, items, row):
                 # ws.cell(row=10, column=i).alignment = copy(cell.alignment)
 
             for i in range(1, 5):
-                ws.cell(row=row, column=i).value = item[i - 1]
+                content = ILLEGAL_CHARACTERS_RE.sub(r'', item[i - 1])
+                ws.cell(row=row, column=i).value = content
     return wb
 
 
